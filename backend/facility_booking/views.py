baff3a01b0a67fd72c337cd3a23a080a9dabfe71
Rajkumar Gupta <rajg87717@gmail.com>
2024-12-17 10:19:34 +0530

    site updated

--- backend/facility_booking/views.py
+++ backend/facility_booking/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Charges
+from .serializers import SlotSerializer
+
+@api_view(['GET'])
+def slots(request):
+    slots = Charges.objects.all()
+    serializer = SlotSerializer(slots, many=True)
+    return Response(serializer.data)
+
+
