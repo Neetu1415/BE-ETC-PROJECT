e14b4292feef92a783196404b610c1b9bbbf628f
Rajkumar Gupta <rajg87717@gmail.com>
2024-12-18 09:57:45 +0530

    site update

--- backend/facility_booking/serializers.py
+++ backend/facility_booking/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Charges
+from sports_facility.models import Sports_complex
 
 class SlotSerializer(serializers.ModelSerializer):
+    uid = serializers.CharField(source= 'sports_complex.uid')
+    sports_complex_name = serializers.CharField(source='sports_complex.name')
+    facility_type = serializers.CharField(source='sports_complex.facility')
     class Meta:
         model = Charges
-        fields = '__all__' 
+        fields = ['uid', 'sports_complex_name', 'facility_type', 'group', 'type', 'rate']
