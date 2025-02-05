9d74569a86ed906218033a65136fd616e39e546f
Rajkumar Gupta <rajg87717@gmail.com>
2025-02-05 09:49:59 +0530

    disable update

--- backend/facility_booking/urls.py
+++ backend/facility_booking/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import slots
-from .views import BookingView , BookingListView
+from .views import BookingView , BookingListView , fully_booked_dates
 
 urlpatterns = [
     path('charges/', slots, name='charges-list'),  # Map the view to the URL
     
     path('bookings/', BookingView.as_view(), name='booking-create'),  # POST
     path('bookings/list/', BookingListView.as_view(), name='booking-list'),  # GET
-    
+     path('bookings/fully-booked/', fully_booked_dates, name='fully-booked-dates'), 
 ]
